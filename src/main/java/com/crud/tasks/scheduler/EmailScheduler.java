b9c0b559710c7803cadb61bbcbff1685fd44d96c
Andrzej <andrzej.splewinski@gmail.com>
2019-10-10 22:35:42 +0200

    creating a new method for sending emails

--- src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
+++ src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
@@ -22,8 +22,8 @@ public class EmailScheduler {
     @Autowired
     private AdminConfig adminConfig;
 
-   // @Scheduled(cron = "0 0 22 * * *")
-    @Scheduled(fixedDelay = 100000)
+    @Scheduled(cron = "0 0 10 * * *")
+    //@Scheduled(fixedDelay = 100000)
     public void sendInformationEmail(){
         long size = taskRepository.count();
         simpleEmailService.send(new Mail(
